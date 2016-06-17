3f2fee919b463fe1a253bdc0a083d710ccd33bfc
Ben <bhturne@g.clemson.edu>
2016-06-17 16:37:33 -0400

    added update method and test

--- src/com/theironyard/Main.java
+++ src/com/theironyard/Main.java
@@ -165,7 +165,7 @@ public class Main {
 
 
 
-            Pizza p = new Pizza(size, crust, sauce, "", toppings);
+            Pizza p = new Pizza(id, size, crust, sauce, "", toppings);
             pizzas.add(p);
         }
         return pizzas;
@@ -181,9 +181,25 @@ public class Main {
     }
 
     public static void deleteBuiltPizza(Connection conn, int pizzaId) throws SQLException {
-        PreparedStatement stmt = conn.prepareStatement("DELETE FROM built WHERE pizza_id = ?");
+        PreparedStatement stmt = conn.prepareStatement("DELETE FROM builtpizza WHERE pizza_id = ?");
         stmt.setInt(1, pizzaId);
         stmt.execute();
+    }
+
+    public static void updatePizza (Connection conn, Pizza pizza) throws SQLException {
+        PreparedStatement stmt = conn.prepareStatement("UPDATE pizzas SET size = ?, crust = ?, sauce = ?, ordername = ? WHERE id = ?");
+        stmt.setString(1, pizza.size);
+        stmt.setString(2, pizza.crust);
+        stmt.setString(3, pizza.sauce);
+        stmt.setString(4, pizza.orderName);
+        stmt.setInt(5, pizza.id);
+        stmt.execute();
+
+        deleteBuiltPizza(conn, pizza.id);
+
+        insertBuiltPizza(conn, pizza.topping, pizza.id);
+
+
     }
 
     public static void main(String[] args) throws SQLException {
@@ -214,6 +230,19 @@ public class Main {
                     insertPizza(conn, pizza);
                     return "";
                 }
+        );
+
+        Spark.put(
+                "/pizza/:id",
+                (request, response) -> {
+                    int id = Integer.valueOf(request.params(":id"));
+                    String body = request.body();
+                    JsonParser p = new JsonParser();
+                    Pizza pizza = p.parse(body, Pizza.class);
+                    updatePizza(conn, pizza);
+                    return "";
+                }
+
         );
 
         Spark.delete(

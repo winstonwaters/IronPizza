d4e90224f3d0f7ef7d244fb45c89be69f59df519
Winston Waters <Winstonw57@gmail.com>
2016-06-18 14:50:56 -0400

    slideshow.js

--- public/apps.js
+++ public/apps.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
 })
 
 var pizzaPage = {
-    url: "",
+    url: "http://tiny-tiny.herokuapp.com/collections/ironPizza",
     pizzaArr: [],
     intit: function() {
         pizzaPage.events();
@@ -32,35 +32,6 @@ var pizzaPage = {
 
             $('input').val("");
         })
-
-
-        // var slideIndex = 1;
-        // showSlides(slideIndex);
-        //
-        // function plusSlides(n) {
-        //   showSlides(slideIndex += n);
-        // }
-        //
-        // function currentSlide(n) {
-        //   showSlides(slideIndex = n);
-        // }
-        //
-        // function showSlides(n) {
-        //   var i;
-        //   var slides = document.getElementsByClassName("mySlides");
-        //   var dots = document.getElementsByClassName("dot");
-        //   if (n > slides.length) {slideIndex = 1}
-        //   if (n < 1) {slideIndex = slides.length}
-        //   for (i = 0; i < slides.length; i++) {
-        //       slides[i].style.display = "none";
-        //   }
-        //   for (i = 0; i < dots.length; i++) {
-        //       dots[i].className = dots[i].className.replace(" active", "hidden");
-        //   }
-        //   slides[slideIndex-1].style.display = "block";
-        //   dots[slideIndex-1].className += " active";
-        // }
-
     },
 
 
